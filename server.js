7beb8a0472f8c6ab6cc2ee052f4077895bb29ab1
Michael Dreesen <michaeldreesen90@gmail.com>
2020-10-01 22:07:11 -0600

    added .json packages and installed express

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 // have to get express to this app
+const express = require('express');
 // have to require path
+const path = require('path');
 // have to require fs
+const fs = require('fs');
+// port number
+const PORT = process.env.PORT || 3001;
+// have to initialize express
+const app = express();
 
 // can write the GET call here
+// app.get('/notes', (req, res) => {
 
+// })
 // can write the POST call here
+
+// Port number
+app.listen(PORT, () => {
+    console.log(`Serving on port ${PORT}`);
+});
